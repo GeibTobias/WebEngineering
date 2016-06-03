168ede6ebf744b9fcdfcf59850016f26318cfacf
Tobias Geib <chi.dor@hotmail.de>
2016-06-03 14:38:22 +0200

    website geht nicht

--- angularController.js
+++ angularController.js
@@ -2,7 +2,7 @@
 var searchInput = [];
 
 
-app.controller("angCtrl", ['$scope', function($scope) {
+app.controller("angCtrl", ['$scope','$http', function($scope, $http) {
     $scope.putin = function(searchTerm) {
         searchInput = angular.copy(searchTerm).split(" ");
     };
@@ -14,11 +14,11 @@ app.controller("angCtrl", ['$scope', function($scope) {
 
     errorCallback = function () {
         console.log("error")
-    }
+    };
 
-    data = JSON.parse("{}");
+    var data = JSON.parse("{}");
 
-    //$http.post('/https://eexcess.joanneum.at/eexcess-privacy-proxy-issuer-1.0-SNAPSHOT/issuer/recommend', data).then(successCallback, errorCallback);
+    $http.post('/https://eexcess.joanneum.at/eexcess-privacy-proxy-issuer-1.0-SNAPSHOT/issuer/recommend', data).then(successCallback, errorCallback);
 
     console.log(searchInput.toString());
 }]);
