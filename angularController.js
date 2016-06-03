752a2ff5bff97f68ef7610d23027b36b0ad924ee
Tobias Geib <chi.dor@hotmail.de>
2016-06-03 14:22:12 +0200

    noch mehr kleinigkeiten

--- angularController.js
+++ angularController.js
@@ -1,9 +1,24 @@
+
 var searchInput = [];
+
 
 app.controller("angCtrl", ['$scope', function($scope) {
     $scope.putin = function(txt) {
         searchInput = angular.copy(txt).split(" ");
     };
+
+
+    successCallback = function () {
+        console.log("success")
+    };
+
+    errorCallback = function () {
+        console.log("error")
+    }
+
+    data = JSON.parse("{}");
+
+    //$http.post('/https://eexcess.joanneum.at/eexcess-privacy-proxy-issuer-1.0-SNAPSHOT/issuer/recommend', data).then(successCallback, errorCallback);
 
     console.log(searchInput.toString());
 }]);
